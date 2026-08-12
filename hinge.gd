2c53f72448a821f5f69c6b359f2bb568faa78334
Abel Saberhagen <abelsaberhagen@gmail.com>
2026-08-12 12:24:22 -0400

    stuff

--- hinge.gd
+++ hinge.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
-@onready var joint: PinJoint2D = $PinJoint2D
+@onready var pinjointfeet1 : PinJoint2D = $"../../Narm/PinJointFeet1"
+@onready var pinjointfeet2 : PinJoint2D = $"../../Narm/PinJointFeet2"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,6 +13,13 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_area_entered(area: Area2D) -> void:
-	pass
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "legsegment8":
+		pinjointfeet1.global_position = global_position
+		pinjointfeet1.node_b = get_parent().get_path()
+	elif body.name == "legsegment6":
+		pinjointfeet2.global_position = global_position
+		pinjointfeet2.node_b = get_parent().get_path()
+	print("body entered")
+		
 	
